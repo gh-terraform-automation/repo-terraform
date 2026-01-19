55230d2d65400790653b97ac435a4c37dcd24b65
SALON Tom <98849116+SalonTom@users.noreply.github.com>
2026-01-19 10:40:33 +0100

    Comment out GitHub Actions secret resource
    Comment out GitHub Actions secret resource configuration.

--- secrets.tf
+++ secrets.tf
@@ -1,7 +1,7 @@
-resource "github_actions_secret" "gh_private_token" {
-  for_each = var.list_repositories
-
-  repository       = each.value
-  secret_name      = "GH_PRIVATE_TOKEN"
-  plaintext_value  = var.gh_private_token
-}
+# resource "github_actions_secret" "gh_private_token" {
+#  for_each = var.list_repositories
+#
+#  repository       = each.value
+#  secret_name      = "GH_PRIVATE_TOKEN"
+#  plaintext_value  = var.gh_private_token
+# }
